d686b24b29ca165ecf3ffe6ff4655988055dbbbf
Chadwick Boulay <chadwick.boulay@gmail.com>
2015-02-16 09:10:13 -0500

    Refactored so repo can be subdir in any project\Plugins .

--- Plugins/PSMovePlugin/Source/PSMovePlugin/Private/PSMovePluginPrivatePCH.h
+++
@@ -1,9 +0,0 @@
-// Copyright 1998-2015 Epic Games, Inc. All Rights Reserved.
-
-#include "IPSMovePlugin.h"
-#include "../../ThirdParty/psmoveapi/include/psmove.h"
-#include "../../ThirdParty/psmoveapi/include/psmove_fusion.h"
-#include "../../ThirdParty/psmoveapi/include/psmove_tracker.h"
-
-// You should place include statements to your module's private header files here.  You only need to
-// add includes for headers that are used in most of your module's source files though.
