4e08aa320e3d92adabf50727ae3afefdbcad8eaf
Chadwick Boulay <chadwick.boulay@gmail.com>
2015-03-24 12:56:16 -0400

    Made all implicit int->bool conversions explicit.

--- Source/PSMove/Private/PSMoveComponent.cpp
+++ Source/PSMove/Private/PSMoveComponent.cpp
@@ -3,7 +3,7 @@
 #include "FPSMove.h"
 
 UPSMoveComponent::UPSMoveComponent(const FObjectInitializer &init)
-: PSMoveID(0)//, RumbleRequest(0)
+: PSMoveID(0), RumbleRequest(0)
 {
     bWantsInitializeComponent = true;
     PrimaryComponentTick.bCanEverTick = true;
@@ -39,52 +39,52 @@ void UPSMoveComponent::TickComponent( float DeltaTime, ELevelTick TickType, FAct
         int Released = FPSMove::Get().GetReleased(PSMoveID);
         
         // Update button member variables
-        TriangleDown = Buttons & PSMove_Button::Btn_TRIANGLE;
-        CircleDown = Buttons & PSMove_Button::Btn_CIRCLE;
-        CrossDown = Buttons & PSMove_Button::Btn_CROSS;
-        SquareDown = Buttons & PSMove_Button::Btn_SQUARE;
-        SelectDown = Buttons & PSMove_Button::Btn_SELECT;
-        StartDown = Buttons & PSMove_Button::Btn_START;
-        PSDown = Buttons & PSMove_Button::Btn_PS;
-        MoveDown = Buttons & PSMove_Button::Btn_MOVE;
+        TriangleDown = (Buttons & PSMove_Button::Btn_TRIANGLE) != 0;
+        CircleDown = (Buttons & PSMove_Button::Btn_CIRCLE) != 0;
+        CrossDown = (Buttons & PSMove_Button::Btn_CROSS) != 0;
+        SquareDown = (Buttons & PSMove_Button::Btn_SQUARE) != 0;
+        SelectDown = (Buttons & PSMove_Button::Btn_SELECT) != 0;
+        StartDown = (Buttons & PSMove_Button::Btn_START) != 0;
+        PSDown = (Buttons & PSMove_Button::Btn_PS) != 0;
+        MoveDown = (Buttons & PSMove_Button::Btn_MOVE) != 0;
         
         // Fire button event elegates
         if (TriangleDown || (Released & PSMove_Button::Btn_TRIANGLE))
         {
-            OnTriangleButton.Broadcast(TriangleDown, Pressed & PSMove_Button::Btn_TRIANGLE, Released & PSMove_Button::Btn_TRIANGLE);
+            OnTriangleButton.Broadcast(TriangleDown, (Pressed & PSMove_Button::Btn_TRIANGLE) != 0, (Released & PSMove_Button::Btn_TRIANGLE) != 0);
         }
         if (CircleDown || (Released & PSMove_Button::Btn_CIRCLE))
         {
-            OnCircleButton.Broadcast(CircleDown, Pressed & PSMove_Button::Btn_CIRCLE, Released & PSMove_Button::Btn_CIRCLE);
+            OnCircleButton.Broadcast(CircleDown, (Pressed & PSMove_Button::Btn_CIRCLE) != 0, (Released & PSMove_Button::Btn_CIRCLE) != 0);
         }
         if (CrossDown || (Released & PSMove_Button::Btn_CROSS))
         {
-            OnCrossButton.Broadcast(CrossDown, Pressed & PSMove_Button::Btn_CROSS, Released & PSMove_Button::Btn_CROSS);
+            OnCrossButton.Broadcast(CrossDown, (Pressed & PSMove_Button::Btn_CROSS) != 0, (Released & PSMove_Button::Btn_CROSS) != 0);
         }
         if (SquareDown || (Released & PSMove_Button::Btn_SQUARE))
         {
-            OnSquareButton.Broadcast(SquareDown, Pressed & PSMove_Button::Btn_SQUARE, Released & PSMove_Button::Btn_SQUARE);
+            OnSquareButton.Broadcast(SquareDown, (Pressed & PSMove_Button::Btn_SQUARE) != 0, (Released & PSMove_Button::Btn_SQUARE) != 0);
         }
         if (SelectDown || (Released & PSMove_Button::Btn_SELECT))
         {
-            OnSelectButton.Broadcast(SelectDown, Pressed & PSMove_Button::Btn_SELECT, Released & PSMove_Button::Btn_SELECT);
+            OnSelectButton.Broadcast(SelectDown, (Pressed & PSMove_Button::Btn_SELECT) != 0, (Released & PSMove_Button::Btn_SELECT) != 0);
         }
         if (StartDown || (Released && PSMove_Button::Btn_START))
         {
-            OnStartButton.Broadcast(StartDown, Pressed & PSMove_Button::Btn_START, Released & PSMove_Button::Btn_START);
+            OnStartButton.Broadcast(StartDown, (Pressed & PSMove_Button::Btn_START) != 0, (Released & PSMove_Button::Btn_START) != 0);
         }
         if (PSDown || (Released & PSMove_Button::Btn_PS))
         {
-            OnPSButton.Broadcast(PSDown, Pressed & PSMove_Button::Btn_PS, Released & PSMove_Button::Btn_PS);
+            OnPSButton.Broadcast(PSDown, (Pressed & PSMove_Button::Btn_PS) != 0, (Released & PSMove_Button::Btn_PS) != 0);
         }
         if (MoveDown || (Released && PSMove_Button::Btn_MOVE))
         {
-            OnMoveButton.Broadcast(MoveDown, Pressed & PSMove_Button::Btn_MOVE, Released & PSMove_Button::Btn_MOVE);
+            OnMoveButton.Broadcast(MoveDown, (Pressed & PSMove_Button::Btn_MOVE) != 0, (Released & PSMove_Button::Btn_MOVE) != 0);
         }
         
         // Trigger
         TriggerValue = FPSMove::Get().GetTrigger(PSMoveID);
-        OnTriggerButton.Broadcast(TriggerValue, Pressed & PSMove_Button::Btn_T, Released & PSMove_Button::Btn_T);
+        OnTriggerButton.Broadcast(TriggerValue, (Pressed & PSMove_Button::Btn_T) != 0, (Released & PSMove_Button::Btn_T) != 0);
         
         // Rumble
         FPSMove::Get().SetRumble(PSMoveID, RumbleRequest);
